604d8fe30afed2213fe4c255ba472973947cde7d
JoshTimAllen <josh-tim_allen@hotmail.com>
2020-11-04 18:45:02 -0500

    Updated MechControls + Added CineMachine
    Added IKController script for future IK stuff

---
+++ Assets/Scripts/MechaAnimate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechaAnimate : MonoBehaviour {
+    Animator animator;
+    void Start() {
+        animator = GetComponentInChildren<Animator>();
+    }
+    public void Fly(bool active) {
+        animator.SetBool("Flying", active);
+    }
+}
