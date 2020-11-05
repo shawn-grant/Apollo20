901162682fd0ab3d90af70688a3ea21d59b6472d
Shawn Grant <shawngrant333@gmail.com>
2020-11-04 19:04:46 -0500

    moved Inputs + updated ship controls

--- Assets/Scripts/ShuttleControls.cs
+++ Assets/Scripts/ShuttleControls.cs
@@ -6,9 +6,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class ShuttleControls : MonoBehaviour
-{ 
+{
+    ShuttleInputActions inputActions;
+    public float moveSpeed = 10, turnSpeed = 5;
+
+    private void Awake()
+    {
+        inputActions = new ShuttleInputActions();
+      
+        //inputActions.Flight.Pitch.started += ctx => braking = true;
+        //inputActions.Flight.Brake.canceled += ctx => braking = false;
+    }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +34,12 @@ public class ShuttleControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //steer values
+        float pitch = moveSpeed * inputActions.Flight.Pitch.ReadValue<float>();
+        float roll = turnSpeed * inputActions.Flight.Roll.ReadValue<float>();
+        float yaw = turnSpeed * inputActions.Flight.Yaw.ReadValue<float>();
+
+        transform.Rotate(pitch, yaw, roll);
+
     }
 }
